838efb087149bb9e11c8405b7aa6e77f3a0132e3
dgulino <drew.gulino@gmail.com>
2016-03-15 21:01:21 -0400

    switched to predefined keyboard layouts

--- src/main.c
+++ src/main.c
@@ -6,11 +6,9 @@ Window *my_window;
 TextLayer *text_layer;
 T3Window * myT3Window;
 
-const char MY_KEYBOARD_LAYOUT[] =
-    "abc\0"  "def\0"  "ghi\0"
-    "jkl\0"  "mno\0"  "pqr\0"
-    "stu\0"  "vwx\0"  "yz ";
-const char * myKeyboardSet[] = {MY_KEYBOARD_LAYOUT};
+const char * keyboardSet1[] = {T3_LAYOUT_LOWERCASE, T3_LAYOUT_UPPERCASE};
+const char * keyboardSet2[] = {T3_LAYOUT_NUMBERS};
+const char * keyboardSet3[] = {T3_LAYOUT_PUNC, T3_LAYOUT_BRACKETS};
 
 void myCloseHandler(const char * text) {
     // Do something
@@ -18,23 +16,18 @@ void myCloseHandler(const char * text) {
 
 void handle_init(void) {
   myT3Window = t3window_create(
-    myKeyboardSet, 2,
-    myKeyboardSet, 1,
-    myKeyboardSet, 2,
+    keyboardSet1, 2,
+    keyboardSet2, 1,
+    keyboardSet3, 2,
     (T3CloseHandler)myCloseHandler);
+  T3_SET_THEME_BLUE(myT3Window);
   
   t3window_show(myT3Window, true);
   
-  //my_window = window_create();
-
-  //text_layer = text_layer_create(GRect(0, 0, 144, 20));
-  //window_stack_push(my_window, true);
 }
 
 void handle_deinit(void) {
   t3window_destroy(myT3Window);
-  //text_layer_destroy(text_layer);
-  //window_destroy(my_window);
 }
 
 int main(void) {
